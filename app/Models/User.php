046f0a18a32a164abf3185615dd8874e89c90f29
Icaro William <icarojobsoficial@gmail.com>
2025-10-31 16:56:28 -0300

    chore: finished laravel setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 // use Laravel\Scout\Searchable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
     use Notifiable;
     // use Searchable; // para full-text search
 
